44422bb34eb59d43d4711b00c8d46f42100ac58f
5f1839842f2fb <57242175+5f1839842f2fb@users.noreply.github.com>
2020-01-24 23:09:53 -0500

    Carousel (without animations)

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,47 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+const carouselMaker = () => {
+  let currentImageIndex = 0;
+  const imageSrcObj = [
+    "./assets/carousel/mountains.jpeg", //0
+    "./assets/carousel/computer.jpeg",  //1
+    "./assets/carousel/trees.jpeg",     //2
+    "./assets/carousel/turntable.jpeg"  //3
+  ]
+  const carouselContainer = document.querySelector('.carousel-container')
+  const carouselDiv = document.createElement('div')
+  carouselDiv.classList.add('carousel')
+  const leftButton = document.createElement('div')
+  leftButton.classList.add('left-button')
+  const rightButton = document.createElement('div')
+  rightButton.classList.add('right-button')
+  const image = document.createElement('img')
+  image.setAttribute('src', imageSrcObj[currentImageIndex])
+  carouselContainer.appendChild(carouselDiv)
+  carouselDiv.appendChild(leftButton)
+  carouselDiv.appendChild(image)
+  carouselDiv.appendChild(rightButton)
+
+  leftButton.addEventListener('click', () => {
+    if(currentImageIndex === 0) {
+      currentImageIndex = 3;
+    }
+    else {
+      currentImageIndex -= 1;
+    }
+    image.setAttribute('src', imageSrcObj[currentImageIndex])
+  })
+  rightButton.addEventListener('click', () => {
+    if(currentImageIndex === 3) {
+      currentImageIndex = 0;
+    }
+    else {
+      currentImageIndex += 1;
+    }
+    image.setAttribute('src', imageSrcObj[currentImageIndex])
+  })
+}
+
+carouselMaker()
