6b5f2f189dbb596d6cbeaf8640eae938d7b738fc
5f1839842f2fb <57242175+5f1839842f2fb@users.noreply.github.com>
2020-01-24 22:12:59 -0500

    Cards

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,45 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(response => {
+        console.log(response)
+        cardMaker(response)
+    })
+    .catch(problem => {
+        console.log(problem)
+    })
+
+const cardMaker = (response) => {
+    let articlesArray = [];
+    for (const key in response.data.articles) {
+        response.data.articles[key].forEach(element => {
+            articlesArray.push(element)
+        })
+    }
+    console.log(articlesArray)
+    let cardContainer = document.querySelector('.cards-container')
+    articlesArray.forEach(element => {
+        const cardDiv = document.createElement('div');
+        cardDiv.classList.add('card')
+        const headlineDiv = document.createElement('div');
+        headlineDiv.classList.add('headline')
+        headlineDiv.textContent = element.headline;
+        const authorDiv = document.createElement('div');
+        authorDiv.classList.add('author');
+        const imgDiv = document.createElement('div');
+        imgDiv.classList.add('img-container');
+        const authorImg = document.createElement('img');
+        authorImg.setAttribute('src', element.authorPhoto);
+        const nameSpan = document.createElement('span');
+        nameSpan.textContent = 'By '+element.authorName;
+        cardContainer.appendChild(cardDiv);
+        cardDiv.appendChild(headlineDiv);
+        cardDiv.appendChild(authorDiv);
+        authorDiv.appendChild(imgDiv);
+        authorDiv.appendChild(nameSpan);
+        imgDiv.appendChild(authorImg);
+    })
+    
+}
